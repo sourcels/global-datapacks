e19c29b592f3215df67063c3690de40fb0b399e2
sourcels <siperdima3@gmail.com>
2025-12-29 05:43:39 +0100

    Added optimisattion.

--- src/main/java/com/shimaper/globaldatapacks/GlobalDatapacks.java
+++ src/main/java/com/shimaper/globaldatapacks/GlobalDatapacks.java
@@ -2,6 +2,7 @@ package com.shimaper.globaldatapacks;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,12 +14,8 @@ public class GlobalDatapacks implements ModInitializer {
     public static final String MOD_ID = "global-datapacks";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
-    private static boolean datapacksCopiedForCurrentSession = false;
-
     @Override
     public void onInitialize() {
-        LOGGER.info("Global Datapacks initialized for Minecraft 1.21.11");
-
         createGlobalDatapacksFolder();
     }
 
@@ -27,37 +24,24 @@ public class GlobalDatapacks implements ModInitializer {
             Path minecraftDir = FabricLoader.getInstance().getGameDir();
             Path datapacksDir = minecraftDir.resolve("datapacks");
 
-            if (!Files.exists(datapacksDir)) {
+            if (!Files.exists(datapacksDir))
                 Files.createDirectories(datapacksDir);
-                LOGGER.info("Created global datapacks folder: {}", datapacksDir);
-            } else {
-                LOGGER.debug("Global datapacks folder already exists: {}", datapacksDir);
-            }
+
         } catch (IOException e) {
-            LOGGER.error("Failed to create global datapacks folder", e);
+            LOGGER.error("[Global Datapacks] Failed to create global datapacks folder", e);
         }
     }
 
-    public static boolean copyDatapacks(Path tempWorldPath, Path minecraftDir) {
-        if (datapacksCopiedForCurrentSession) {
-            LOGGER.debug("Datapacks already copied for this world creation session");
-            return false;
-        }
-
+    public static void copyDatapacks(Path tempWorldPath, Path minecraftDir) {
         Path globalDatapacksDir = minecraftDir.resolve("datapacks");
 
         if (!Files.exists(globalDatapacksDir)) {
-            LOGGER.info("Global datapacks folder not found: {}", globalDatapacksDir);
-            LOGGER.info("Create .minecraft/datapacks folder and place your datapacks there");
-            return false;
+            LOGGER.info("[Global Datapacks] Folder not found: {}", globalDatapacksDir);
+            LOGGER.info("[Global Datapacks] Create .minecraft/datapacks folder and place your datapacks there");
+            return;
         }
 
         try {
-            if (!Files.exists(tempWorldPath)) {
-                Files.createDirectories(tempWorldPath);
-                LOGGER.info("Created datapacks folder: {}", tempWorldPath);
-            }
-
             int copiedCount = 0;
 
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(globalDatapacksDir)) {
@@ -67,41 +51,33 @@ public class GlobalDatapacks implements ModInitializer {
 
                     if (Files.isDirectory(sourceDatapack)) {
                         copyDirectory(sourceDatapack, targetDatapack);
-                        LOGGER.info("Copied datapack folder: {}", datapackName);
+                        LOGGER.info("[Global Datapacks] Copied datapack folder: {}", datapackName);
                         copiedCount++;
                     } else if (datapackName.endsWith(".zip")) {
                         Files.copy(sourceDatapack, targetDatapack, StandardCopyOption.REPLACE_EXISTING);
-                        LOGGER.info("Copied datapack zip: {}", datapackName);
+                        LOGGER.info("[Global Datapacks] Copied datapack zip: {}", datapackName);
                         copiedCount++;
                     }
                 }
             }
 
             if (copiedCount > 0) {
-                LOGGER.info("Successfully copied {} datapack(s) from {} to {}",
+                LOGGER.info("[Global Datapacks] Successfully copied {} datapack(s) from {} to {}",
                         copiedCount, globalDatapacksDir, tempWorldPath);
-                datapacksCopiedForCurrentSession = true;
-                return true;
             } else {
-                LOGGER.info("No datapacks found in {}", globalDatapacksDir);
-                return false;
+                LOGGER.info("[Global Datapacks] No datapacks found in {}", globalDatapacksDir);
             }
 
         } catch (IOException e) {
-            LOGGER.error("Error copying datapacks", e);
-            return false;
+            LOGGER.error("[Global Datapacks] Error copying datapacks", e);
         }
     }
 
-    public static void resetCopyFlag() {
-        datapacksCopiedForCurrentSession = false;
-        LOGGER.debug("Datapack copy flag reset");
-    }
-
     private static void copyDirectory(Path source, Path target) throws IOException {
-        Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
+        Files.walkFileTree(source, new SimpleFileVisitor<>() {
+
             @Override
-            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs)
+            public @NotNull FileVisitResult preVisitDirectory(@NotNull Path dir, @NotNull BasicFileAttributes attrs)
                     throws IOException {
                 Path targetDir = target.resolve(source.relativize(dir));
                 Files.createDirectories(targetDir);
@@ -109,7 +85,7 @@ public class GlobalDatapacks implements ModInitializer {
             }
 
             @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs)
+            public @NotNull FileVisitResult visitFile(@NotNull Path file, @NotNull BasicFileAttributes attrs)
                     throws IOException {
                 Path targetFile = target.resolve(source.relativize(file));
                 Files.copy(file, targetFile, StandardCopyOption.REPLACE_EXISTING);
--- src/main/java/com/shimaper/globaldatapacks/mixin/CreateWorldScreenMixin.java
+++ src/main/java/com/shimaper/globaldatapacks/mixin/CreateWorldScreenMixin.java
@@ -1,72 +1,28 @@
 package com.shimaper.globaldatapacks.mixin;
 
+import java.nio.file.Path;
+
 import com.shimaper.globaldatapacks.GlobalDatapacks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.worldselection.CreateWorldScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.io.File;
-import java.nio.file.Path;
 
 
 @Mixin(CreateWorldScreen.class)
 public abstract class CreateWorldScreenMixin {
-    @Inject(method = "openDataPackSelectionScreen", at = @At("HEAD"))
-    private void onOpenPackScreen(CallbackInfo ci) {
-        try {
-            Path tempDir = Path.of(System.getProperty("java.io.tmpdir"));
-            Minecraft client = Minecraft.getInstance();
-            Path minecraftDir = client.gameDirectory.toPath();
 
-            File[] tempWorldDirs = tempDir.toFile().listFiles((dir, name) ->
-                    name.startsWith("mcworld-") && new File(dir, name).isDirectory()
-            );
+    @Inject(method = "getOrCreateTempDataPackDir", at = @At("RETURN"))
+    private void afterCreateTempDir(CallbackInfoReturnable<Path> cir) {
+        Path tempPath = cir.getReturnValue();
 
-            if (tempWorldDirs != null && tempWorldDirs.length > 0) {
-                File newestTempWorld = null;
-                long newestTime = 0;
+        Minecraft client = Minecraft.getInstance();
+        Path minecraftDir = client.gameDirectory.toPath();
 
-                GlobalDatapacks.LOGGER.debug("Found {} temporary world folder(s)", tempWorldDirs.length);
-
-                for (File tempWorld : tempWorldDirs) {
-                    long lastModified = tempWorld.lastModified();
-                    GlobalDatapacks.LOGGER.debug("Checking folder: {} (modified: {})",
-                            tempWorld.getName(), lastModified);
-
-                    if (lastModified > newestTime) {
-                        newestTime = lastModified;
-                        newestTempWorld = tempWorld;
-                    }
-                }
-
-                if (newestTempWorld != null) {
-                    Path tempWorldPath = newestTempWorld.toPath();
-                    GlobalDatapacks.LOGGER.info("Selected newest temporary world folder: {} (age: {} ms ago)",
-                            tempWorldPath.getFileName(),
-                            System.currentTimeMillis() - newestTime);
-
-                    boolean copied = GlobalDatapacks.copyDatapacks(tempWorldPath, minecraftDir);
-
-                    if (copied) {
-                        GlobalDatapacks.LOGGER.info("Datapacks are ready to use!");
-                    }
-                }
-            } else {
-                GlobalDatapacks.LOGGER.warn("No temporary world folders found in {}", tempDir);
-                GlobalDatapacks.LOGGER.warn("This might happen if you opened Data Packs screen before the temp folder was created");
-            }
-
-        } catch (Exception e) {
-            GlobalDatapacks.LOGGER.error("Error while trying to copy datapacks", e);
-        }
+        GlobalDatapacks.copyDatapacks(tempPath, minecraftDir);
     }
 
-    @Inject(method = "onClose", at = @At("HEAD"))
-    private void onScreenClosed(CallbackInfo ci) {
-        GlobalDatapacks.resetCopyFlag();
-        GlobalDatapacks.LOGGER.debug("Create world screen closed");
-    }
 }
