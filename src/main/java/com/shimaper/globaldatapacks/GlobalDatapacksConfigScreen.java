6ef35165a3a615669e1dd98e6a0e367e0de6f601
sourcels <siperdima3@gmail.com>
2026-01-20 19:09:41 +0100

    Switched to 26.1-snapshot-3, also fixed problem with blur in ConfigMenu

--- src/main/java/com/shimaper/globaldatapacks/GlobalDatapacksConfigScreen.java
+++ src/main/java/com/shimaper/globaldatapacks/GlobalDatapacksConfigScreen.java
@@ -57,7 +57,7 @@ public class GlobalDatapacksConfigScreen extends Screen {
 
     @Override
     public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
-        this.renderBackground(guiGraphics, mouseX, mouseY, partialTick);
         super.render(guiGraphics, mouseX, mouseY, partialTick);
+        guiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 0xFFFFFF);
     }
 }
